388a8e13ccd05635ced35c146017f1c791b7f84c
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-06-16 23:09:17 +0530

    Time: 4 ms (13.46%), Space: 2 MB (13.46%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.go
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.go
@@ -1,13 +1,3 @@
 func numberOfMatches(n int) int {
-    match:=0
-    for n>1 {
-        if n%2==0 {
-            match+=n/2
-            n=n/2
-        }else {
-            match+=n/2
-            n=n/2+1
-        }
-    }
-    return match
+    return n-1
 }
