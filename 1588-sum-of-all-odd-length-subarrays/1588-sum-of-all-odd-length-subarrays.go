38c17d247dc1b09c1afddd6822edc9783a25efdc
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-06-17 21:23:43 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (100.00%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.go
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.go
@@ -1,14 +1,15 @@
 func sumOddLengthSubarrays(arr []int) int {
-    sum:=0
-    for i:=0;i<len(arr);i++ {
-        for j:=i+1;j<=len(arr);j++ {
-            n:=len(arr[i:j])
-            if n%2!=0 {
-                for k:=0;k<n;k++ {
-                    sum+=arr[i:j][k]
-                }
-            }
+    result:=0
+    n:=len(arr)
+    for i:=0;i<n;i++ {
+        start:=n-i
+        end:=i+1
+        total:=start*end
+        odd:=total/2
+        if total%2!=0 {
+            odd++
         }
+        result+=odd*arr[i]
     }
-    return sum
+    return result
 }
