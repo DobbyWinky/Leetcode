2d00b3e9f1c50db34036c14b8643fe46d2d60969
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-06 18:58:01 -0500

    Time: 74 ms (93.33%), Space: 9.7 MB (82.85%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.go
@@ -1,28 +1,33 @@
 /*
-Time - O(n)
-Space - O(n)
+    Time - O(nlogn)
+    Space - O(1)
 */
 func longestConsecutive(nums []int) int {
-    if len(nums)==0 {
-        return 0
+    if len(nums)==1 {
+        return 1
     }
-    hash:=make(map[int]bool)
-    for _, num:=range nums {
-        hash[num]=true
-    }
-    max:=1
-    for k, _:=range hash {
-        if !hash[k-1] {
-            start:=k
-            ans:=1
-            for hash[start+1] {
-                ans++
-                start++
-            }
-            if ans>max {
-                max=ans
+    sort.Ints(nums)
+    max:=0
+    for i:=0;i<len(nums)-1;i++ {
+        k:=1
+        for i<len(nums)-1 {
+            if nums[i]+1==nums[i+1] {
+                k++
+                i++
+            }else if nums[i]==nums[i+1] {
+                i++
+            }else {
+                break
             }
         }
+        max=maximum(max, k)
     }
     return max
+}
+
+func maximum(i,j int) int {
+    if i>j {
+        return i
+    }
+    return j
 }
