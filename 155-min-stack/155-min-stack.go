eac3ced82150c1ec51f020477cdcc61311170479
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-05 23:25:19 -0500

    Time: 18 ms (89.59%), Space: 9.2 MB (11.46%) - LeetHub

--- 155-min-stack/155-min-stack.go
+++ 155-min-stack/155-min-stack.go
@@ -1,6 +1,7 @@
 /*
 Time - O(1)
 Space - O(2n)
+Store the value and the mininum value in the same stack
 */
 type MinStack struct {
     stack [][2]int
@@ -11,7 +12,8 @@ func Constructor() MinStack {
     return MinStack{make([][2]int, 0)}
 }
 
-
+// Check if the value being pushed is less than the current min which is maintained in the stack
+// If the value is less, while pushing overwrite the current min in the next value being pushed
 func (this *MinStack) Push(val int)  {
     if len(this.stack)==0 {
         this.stack=append(this.stack, [2]int{val, val})
