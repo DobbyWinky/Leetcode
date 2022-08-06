aa41bd0b28fa8e56a7d0e43a6d2ba8dbe76a5250
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-06 17:08:58 -0500

    Time: 51 ms (34.35%), Space: 8.2 MB (36.57%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -1,35 +1,18 @@
 /*
 Time - O(n)
-Space - O(n)
+Space - O(1)
 */
 func productExceptSelf(nums []int) []int {
-    prefix:=make([]int, len(nums))
-    suffix:=make([]int, len(nums)) 
-    prefix[0]=nums[0]
-    for i:=1;i<len(nums);i++ {
-        prefix[i]=prefix[i-1]*nums[i]
+    n:=len(nums)
+    ans:=make([]int, n)
+    ans[0]=1
+    for i:=1;i<n;i++ {
+        ans[i]=ans[i-1]*nums[i-1]
     }
-    suffix[len(suffix)-1]=nums[len(nums)-1]
-    for i:=len(nums)-2;i>=0;i-- {
-        suffix[i]=suffix[i+1]*nums[i]
+    r:=1
+    for i:=n-1;i>=0;i-- {
+        ans[i]*=r
+        r*=nums[i]
     }
-    fmt.Println(prefix)
-    fmt.Println(suffix)
-    
-    for i:=0;i<len(nums);i++ {
-        var prev int
-        var next int
-        if i==0 {
-            prev=1
-        }else {
-            prev=prefix[i-1]
-        }
-        if i==len(nums)-1 {
-            next=1
-        }else {
-            next=suffix[i+1]
-        }
-        nums[i]=prev*next
-    }
-    return nums
+    return ans
 }
