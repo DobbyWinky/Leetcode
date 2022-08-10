a6e022c51451a53fdab6f0cb612af389f1471bf5
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-09 22:51:50 -0500

    Time: 8 ms (94.66%), Space: 5.3 MB (72.95%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -1,38 +1,30 @@
 /*
 Time - O(n)
-Space - O(n)
+Space - O(1)
 */
-
 func trap(height []int) int {
     n:=len(height)
-    leftMax:=make([]int,n)
-    rightMax:=make([]int, n)
-    lMax:=0
-    for i:=0;i<n;i++ {
-        leftMax[i]=lMax
-        if height[i]>lMax {
-            lMax=height[i]
-        }
-    }
-    rMax:=0
-    for i:=n-1;i>=0;i-- {
-        rightMax[i]=rMax
-        if height[i]>rMax {
-            rMax=height[i]
-        }
-    }
     water:=0
-    for i:=0;i<n;i++ {
-        currWater:=min(leftMax[i], rightMax[i])-height[i]
-        if currWater>0 {
-            water+=currWater
+    l:=0
+    r:=n-1
+    maxL:=height[l]
+    maxR:=height[r]
+    for l<r {
+        if maxL<maxR {
+            l++
+            water+=max(maxL-height[l],0)
+            maxL=max(maxL, height[l])
+        }else {
+            r--
+            water+=max(maxR-height[r], 0)
+            maxR=max(maxR, height[r])
         }
     }
     return water
 }
 
-func min(i,j int) int {
-    if i<j {
+func max(i, j int) int {
+    if i>j {
         return i
     }
     return j
