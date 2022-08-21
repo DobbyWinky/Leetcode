b8b18015c6ab0f5c57245a711a0fa443d29a889b
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-21 15:31:18 -0500

    Time: 5 ms (59.59%), Space: 2.9 MB (65.57%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.go
+++ 125-valid-palindrome/125-valid-palindrome.go
@@ -1,14 +1,14 @@
 import "strings"
 func isPalindrome(s string) bool {
-    s=strings.ToLower(s)
     i:=0
     j:=len(s)-1
+    s=strings.ToLower(s)
     for i<j {
         if !unicode.IsLetter(rune(s[i])) && !unicode.IsDigit(rune(s[i])) {
             i++
             continue
         }
-        if !unicode.IsLetter(rune(s[j])) && !unicode.IsDigit(rune(s[j])) {
+        if !unicode.IsLetter(rune(s[j])) && !unicode.IsDigit(rune(s[j])){
             j--
             continue
         }
