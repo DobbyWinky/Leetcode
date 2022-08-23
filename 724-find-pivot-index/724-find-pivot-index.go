4cf55bbbea1e22efe2d5ff113bad2f016d769f75
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-23 11:56:12 -0500

    Time: 43 ms (26.64%), Space: 6.7 MB (11.64%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.go
+++ 724-find-pivot-index/724-find-pivot-index.go
@@ -1,16 +1,13 @@
 func pivotIndex(nums []int) int {
-    prefix:=make([]int, len(nums))
     suffix:=make([]int, len(nums))
-    prefix[0]=nums[0]
-    for i:=1;i<len(nums);i++ {
-        prefix[i]=prefix[i-1]+nums[i]
-    }
     suffix[len(nums)-1]=nums[len(nums)-1]
     for i:=len(nums)-2; i>=0;i-- {
         suffix[i]=suffix[i+1]+nums[i]
     }
+    curr:=0
     for i:=0;i<len(nums);i++ {
-        if prefix[i]==suffix[i] {
+        curr+=nums[i]
+        if curr==suffix[i] {
             return i
         }
     }
