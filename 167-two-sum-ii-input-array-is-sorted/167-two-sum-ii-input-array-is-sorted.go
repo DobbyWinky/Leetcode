c5b5be96ab259da275e482ab79e28ab53a38e117
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-22 19:29:19 -0500

    Time: 12 ms (85.30%), Space: 5.4 MB (55.39%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
@@ -5,13 +5,14 @@ Space - O(1)
 func twoSum(nums []int, target int) []int {
     lo:=0
     hi:=len(nums)-1
-    for lo<=hi {
+    for lo<hi {
         if nums[lo]+nums[hi]==target {
             return []int{lo+1, hi+1}
-        }else if nums[lo]+nums[hi]>target {
-            hi--
-        }else {
+        }
+        if nums[lo]+nums[hi]<target {
             lo++
+        }else {
+            hi--
         }
     }
     return []int{-1,-1}
