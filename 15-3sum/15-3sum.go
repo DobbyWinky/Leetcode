5ef285ecaf3b0ac2182ac6cfae22b74d6d359bff
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-22 20:15:00 -0500

    Time: 1891 ms (9.30%), Space: 9.6 MB (12.18%) - LeetHub

--- 15-3sum/15-3sum.go
+++ 15-3sum/15-3sum.go
@@ -1,29 +1,27 @@
 /*
-No sort algo
 Time - O(n2)
 Space - O(n)
 */
 func threeSum(nums []int) [][]int {
-    res:=make(map[[3]int]bool)
-    for i:=0;i<len(nums);i++ {
-        seen:=make(map[int]bool)
-        for j:=i+1;j<len(nums);j++ {
-            // Check for duplicates and chexk if the compliment exists in the hash map
-            // If the compliment exists, put the result into a map to avoid duplicates in the result
-            if seen[-(nums[i]+nums[j])] {
-                temp:=[]int{nums[i],nums[j],-(nums[i]+nums[j])}
-                // This sorting was required since the leetcode solution was expected to be sorted. This sorting takes O(1) since it sorts only 3 elements at a given point of time
-                sort.Ints(temp)
-                res[[3]int{temp[0],temp[1],temp[2]}]=true
-            }
-            seen[nums[j]]=true
-        } 
-    }
-    
-    // Convert the map answer constructed in the previous steps to a slice which is expected by the leetcode solution
     ans:=make([][]int, 0)
-    for k,_:=range res {
-        ans=append(ans, []int{k[0],k[1],k[2]})
+    n:=len(nums)
+    freq:=make(map[int]int)
+    for i, num:=range nums {
+        freq[num]=i
+    }
+    resMap:=make(map[[3]int]bool)
+    for i:=0;i<n;i++ {
+        for j:=i+1;j<n;j++ {
+            complement:=-(nums[i]+nums[j])
+            if val,ok:=freq[complement] ; ok && val!=i && val!=j {
+                val:=[]int{nums[i], nums[j], complement}
+                sort.Ints(val)
+                if !resMap[[3]int{val[0], val[1], val[2]}] {
+                    ans=append(ans, []int{nums[i], nums[j], complement})
+                }
+                resMap[[3]int{val[0], val[1], val[2]}]=true
+            }
+        }
     }
     return ans
 }
