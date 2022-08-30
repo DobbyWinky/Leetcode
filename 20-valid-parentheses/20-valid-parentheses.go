758cae9a44c2e24c0f90dc75db2223104bed360c
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-08-29 19:22:15 -0500

    Time: 0 ms (100.00%), Space: 2 MB (45.98%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.go
+++ 20-valid-parentheses/20-valid-parentheses.go
@@ -3,32 +3,24 @@ Time - O(n)
 Space - O(n)
 */
 func isValid(s string) bool {
-    stack:=make([]byte,0) 
+    if len(s)==0 {
+        return true
+    }
+    stack:=make([]byte, 0)
     for i:=0;i<len(s);i++ {
         if len(stack)==0 || s[i]=='(' || s[i]=='[' || s[i]=='{'{
             stack=append(stack, s[i])
+            continue
         }
-        if s[i]==')' {
-            if stack[len(stack)-1]!='(' {
-                return false
-            }
-            stack=stack[:len(stack)-1]
-        }
-        if s[i]=='}' {
-            if stack[len(stack)-1]!='{' {
-                return false
-            }
-            stack=stack[:len(stack)-1]
-        }
-        if s[i]==']' {
-            if stack[len(stack)-1]!='[' {
-                return false
-            }
+        if s[i]==')' && stack[len(stack)-1]!='(' {
+            return false
+        }else if s[i]=='}' && stack[len(stack)-1]!='{' {
+            return false
+        }else if s[i]==']' && stack[len(stack)-1]!='[' {
+            return false
+        }else {
             stack=stack[:len(stack)-1]
         }
     }
-    if len(stack)==0 {
-        return true
-    }
-    return false
+    return len(stack)==0
 }
