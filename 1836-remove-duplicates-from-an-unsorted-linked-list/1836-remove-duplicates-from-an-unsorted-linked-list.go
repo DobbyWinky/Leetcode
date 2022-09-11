c99b621906e7db665fcca809ea9203e0f55c540e
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-09-11 00:36:43 -0500

    Time: 543 ms (20.83%), Space: 12.8 MB (83.33%) - LeetHub

---
+++ 1836-remove-duplicates-from-an-unsorted-linked-list/1836-remove-duplicates-from-an-unsorted-linked-list.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicatesUnsorted(head *ListNode) *ListNode {
+    temp:=head
+    duplicate:=make(map[int]int)
+    for head!=nil {
+        duplicate[head.Val]++
+        head=head.Next
+    }
+    temp2:=new(ListNode)
+    temp3:=temp2
+    temp2.Next=temp
+    if duplicate[temp2.Next.Val]>1 {
+        temp2.Next=temp2.Next.Next
+    }
+    for temp2!=nil && temp2.Next!=nil{
+        if duplicate[temp2.Next.Val]>1 {
+            temp2.Next=temp2.Next.Next
+        }else {
+            temp2=temp2.Next
+        }
+    }
+    return temp3.Next
+}
