228a74d445fb82671f48531969de63ef0c67e925
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-09-15 21:34:07 -0500

    Time: 6 ms (27.99%), Space: 2.6 MB (51.80%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.go
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.go
@@ -10,17 +10,22 @@ Time - O(n)
 Space - O(n)
 */
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    if l1==nil {
-        return l2
+    prev:=new(ListNode)
+    newList:=prev
+    for l1!=nil && l2!=nil {
+        if l1.Val<l2.Val {
+            prev.Next=l1
+            l1=l1.Next
+        }else {
+            prev.Next=l2
+            l2=l2.Next
+        }
+        prev=prev.Next
     }
-    if l2==nil {
-        return l1
-    }
-    if l1.Val<l2.Val {
-        l1.Next=mergeTwoLists(l1.Next, l2)
-        return l1
+    if l1!=nil {
+        prev.Next=l1
     }else {
-        l2.Next=mergeTwoLists(l1, l2.Next)
-        return l2
+        prev.Next=l2
     }
+    return newList.Next
 }
