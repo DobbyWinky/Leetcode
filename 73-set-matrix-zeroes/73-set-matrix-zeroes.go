dea36d4fee0fb07a296ce94b071064f168e2edc7
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-10-08 23:16:46 -0500

    Time: 24 ms (48.69%), Space: 7 MB (8.64%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.go
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.go
@@ -3,24 +3,34 @@ Time - O(m*n)
 Space - O(m+n)
 */
 func setZeroes(matrix [][]int)  {
-    rowMap:=make([]int, 0)
-    colMap:=make([]int, 0)
+    zeroRow:=1
     for i:=0;i<len(matrix);i++ {
         for j:=0;j<len(matrix[0]);j++ {
             if matrix[i][j]==0 {
-                rowMap=append(rowMap, i)
-                colMap=append(colMap, j)
+                if i==0 {
+                    zeroRow=0
+                }else {
+                    matrix[i][0]=0
+                }
+                matrix[0][j]=0
             }
         }
     }
-    for i:=0;i<len(rowMap);i++ {
-        for j:=0;j<len(matrix[0]);j++ {
-            matrix[rowMap[i]][j]=0
+    for i:=1;i<len(matrix);i++ {
+        for j:=1;j<len(matrix[0]);j++ {
+            if matrix[i][0]==0 || matrix[0][j]==0{
+                matrix[i][j]=0
+            }
         }
     }
-    for j:=0;j<len(colMap);j++ {
+    if matrix[0][0]==0 {
         for i:=0;i<len(matrix);i++ {
-            matrix[i][colMap[j]]=0
+            matrix[i][0]=0
+        }
+    }
+    if zeroRow==0 {
+        for j:=0;j<len(matrix[0]);j++ {
+            matrix[0][j]=0
         }
     }
 }
