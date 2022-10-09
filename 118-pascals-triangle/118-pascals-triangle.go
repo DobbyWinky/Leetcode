fdff60e74b1f3eda55cc97b9f689eec6c53dcd52
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-10-09 00:03:54 -0500

    Time: 2 ms (41.91%), Space: 2.1 MB (56.36%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.go
+++ 118-pascals-triangle/118-pascals-triangle.go
@@ -3,15 +3,18 @@ Time - O(numRows^2)
 Space - O(1)
 */
 func generate(numRows int) [][]int {
-    ans:=make([][]int, 0)
-    for i:=1;i<=numRows;i++ {
-        row:=make([]int, i)
-        row[0]=1
-        row[len(row)-1]=1
-        for j:=1;j<i-1;j++ {
-            row[j]=ans[i-2][j-1]+ans[i-2][j]
-        }
-        ans=append(ans, row)
+    dp:=make([][]int, numRows)
+    for i:=0;i<numRows;i++ {
+        dp[i]=make([]int, i+1)
     }
-    return ans
+    for i:=0;i<numRows;i++ {
+        for j:=0;j<=i;j++ {
+            if j==0 || i==j {
+                dp[i][j]=1
+            }else{
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+            }
+        }
+    }
+    return dp
 }
