d2ed8cba5f07acff906bf4735a7088ea69be8c5e
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-10-28 19:36:19 -0500

    Time: 7 ms (86.96%), Space: 4.3 MB (65.22%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.go
+++ 0243-shortest-word-distance/0243-shortest-word-distance.go
@@ -1,28 +1,33 @@
-func shortestDistance(wordsDict []string, word1 string, word2 string) int {
-    index:=make(map[string][]int)
-    for i:=0;i<len(wordsDict);i++ {
-        index[wordsDict[i]]=append(index[wordsDict[i]], i)
-    }
-    first:=index[word1]
-    second:=index[word2]
-    ans:=9999999
-    for i:=0;i<len(first);i++ {
-        for j:=0;j<len(second);j++ {
-            ans=min(ans, abs(first[i]-second[j]))
+func shortestDistance(wordDict []string, word1 string, word2 string) int {
+    wordMap:=make(map[string][]int)
+    for i:=0;i<len(wordDict);i++ {
+        if wordDict[i]==word1 || wordDict[i]==word2 {
+            wordMap[wordDict[i]]=append(wordMap[wordDict[i]], i+1)
         }
     }
-    return ans
-}
-
-func min(i,j int) int {
-    if i<j {
-        return i
+    list1:=wordMap[word1]
+    list2:=wordMap[word2]
+    i:=0
+    j:=0
+    min:=999999999999
+    for i<len(list1) && j<len(list2) {
+        if abs(list1[i]-list2[j])<min {
+            min=abs(list1[i]-list2[j])
+        }
+        if list1[i]==list2[j] {
+            return 0
+        }
+        if list1[i]<list2[j] {
+            i++
+        }else {
+            j++
+        }
     }
-    return j
+    return min
 }
 
 func abs(i int) int {
-    if i<0{
+    if i<0 {
         return -i
     }
     return i
