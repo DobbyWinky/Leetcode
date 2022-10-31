30a3e7974fd8f5b949cacca336df56edc1c30ed3
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-10-31 17:13:43 -0500

    Time: 57 ms (47.93%), Space: 8.9 MB (7.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -5,19 +5,15 @@ Space - O(n)
 
 func productExceptSelf(nums []int) []int {
     prefix:=make([]int, len(nums))
-    suffix:=make([]int, len(nums))
     prefix[0]=1
     prefix[1]=nums[0]
     for i:=2;i<len(nums);i++ {
         prefix[i]=nums[i-1]*prefix[i-1]
     }
-    suffix[len(suffix)-1]=1
-    suffix[len(suffix)-2]=nums[len(nums)-1]
-    for j:=len(nums)-3;j>=0;j-- {
-        suffix[j]=suffix[j+1]*nums[j+1]
-    }
-    for i:=0;i<len(prefix);i++ {
-        prefix[i]*=suffix[i]
+    right:=1
+    for j:=len(nums)-1;j>=0;j-- {
+        prefix[j]*=right
+        right*=nums[j]
     }
     return prefix
 }
