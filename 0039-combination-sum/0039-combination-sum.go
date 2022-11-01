06a7246fddf31a37b585bbcc8bc5c01a3cd5c205
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-11-01 10:36:07 -0500

    Time: 4 ms (73.63%), Space: 3.1 MB (61.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.go
+++ 0039-combination-sum/0039-combination-sum.go
@@ -1,25 +1,21 @@
-/*
-Time - O(2^t)
-Space - O(t)
-*/
-
 func combinationSum(candidates []int, target int) [][]int {
     res:=make([][]int, 0)
-    var dfs func(i, start int, curr []int) 
-    dfs=func(i, start int, curr []int) {
+    var dfs func(start, index int, curr []int) 
+    dfs = func(start, index int, curr []int){
         if start==target {
             currCopy:=make([]int, len(curr))
             copy(currCopy, curr)
             res=append(res, currCopy)
-            return 
-        }
-        if i>=len(candidates) || start>target {
             return
         }
-        curr=append(curr, candidates[i])
-        dfs(i, start+candidates[i], curr)
-        curr=curr[:len(curr)-1]
-        dfs(i+1, start, curr)
+        if start>target {
+            return
+        }
+        for i:=index;i<len(candidates);i++ {
+            curr=append(curr, candidates[i])
+            dfs(start+candidates[i], i, curr)
+            curr=curr[:len(curr)-1]
+        }
     }
     curr:=make([]int, 0)
     dfs(0,0,curr)
