05d9b89993884558a999d238c857a2f394543138
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-11-01 10:56:45 -0500

    Time: 45 ms (46.04%), Space: 7.1 MB (52.35%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.go
@@ -0,0 +1,28 @@
+func merge(intervals [][]int) [][]int {
+    sort.Slice(intervals, func(i,j int)bool {
+        if intervals[i][0]<intervals[j][0] {
+            return true
+        }
+        return false
+    })
+    res:=make([][]int, 0)
+    for i:=0;i<len(intervals);i++ {
+        if len(res)==0 {
+            res=append(res, intervals[i])
+            continue
+        }
+        if intervals[i][0]<=res[len(res)-1][1] {
+            res[len(res)-1][1]=max(res[len(res)-1][1], intervals[i][1])
+        }else {
+            res=append(res, intervals[i])
+        }
+    }
+    return res
+}
+
+func max(i,j int) int {
+    if i>j {
+        return i
+    }
+    return j
+}
