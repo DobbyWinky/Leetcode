0d8ccb26e1780cb5c172f2e7b251be74831aa5c2
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2022-11-03 10:49:42 -0500

    Time: 68 ms (44.27%), Space: 11.6 MB (5.15%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.go
+++ 0189-rotate-array/0189-rotate-array.go
@@ -1,16 +1,19 @@
 /*
-Time-O(n*k)
-Space-O(1)
+Time - O(n)
+Space - O(1)
 */
 func rotate(nums []int, k int)  {
-    for i:=0;i<k;i++ {
-        rotatedVal:=nums[len(nums)-1]
-        prev:=nums[0]
-        for i:=1;i<len(nums);i++ {
-            curr:=nums[i]
-            nums[i]=prev
-            prev=curr
-        }
-        nums[0]=rotatedVal
+    k=k%len(nums)
+    reverse(nums)
+    reverse(nums[:k])
+    reverse(nums[k:])
+}
+func reverse(nums []int) {
+    i:=0
+    j:=len(nums)-1
+    for i<=j {
+        nums[i], nums[j]=nums[j], nums[i]
+        i++
+        j--
     }
 }
