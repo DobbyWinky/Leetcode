204c69189d61a06e82b114ee7abd31d0de4fcca7
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2023-06-13 21:02:00 -0700

    Time: 385 ms (5.10%), Space: 19.4 MB (48.74%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,9 +1,16 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
+        k=10000
+        freq=[0]*(2*k+1)
+        for num in nums:
+            freq[num+k]+=1
+        is_even=True
         ans=0
-        n=len(nums)
-        for i in range(0, n, 2):
-            ans+=nums[i]
+        for i in range(2*k+1):
+            while freq[i]>0:
+                if is_even:
+                    ans+=i-k
+                is_even=not is_even
+                freq[i]-=1
         return ans
         
