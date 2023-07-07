c5287a1f034a1583d8bf582032fd9a47c3c19efe
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2023-07-07 16:29:36 -0700

    Time: 45 ms (87.67%), Space: 16.4 MB (41.50%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,36 +1,25 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         s=s.strip()
-        new_s=s.lstrip("0")
-        n=len(new_s)
+        n=len(s)
         if n==0:
             return 0
-        if len(new_s)!=len(s) and (new_s[0]=='+' or new_s[0]=='-'):
-            return 0
-        s=new_s
+        i=0
         sign=0
-        digitFound=False
-        signFound=False
         if s[0]=='-':
             sign=1
+            i+=1
+        if s[0]=='+':
+            i+=1
         prev=0
         ans=0
-        for i in range(n):
+        while i<n:
             if not s[i].isdigit():
-                if not digitFound and s[i]!='+' and s[i]!='-':
-                    return 0
-                elif s[i]!='+' and s[i]!='-':
-                    break
-                elif signFound and not digitFound:
-                    return 0
-                elif signFound and digitFound:
-                    break
-                if s[i]=='+' or s[i]=='-':
-                    signFound=True
+                break
             else:
-                digitFound=True
                 ans=prev*10+int(s[i])
                 prev=ans
+            i+=1
         if sign:
             ans=ans*-1
         if ans>2147483647:
