80250402ce418eb07d57cda31192019acdf29be5
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2023-07-10 19:14:13 -0700

    Time: 44 ms (71.96%), Space: 16.2 MB (72.56%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,22 +1,31 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle=="":
-            return 0
-        if len(haystack)<len(needle):
-            return -1
+        lps=[0]*len(needle)
+        i=1
+        prev=0
+        while i<len(needle):
+            if needle[i]==needle[prev]:
+                lps[i]=prev+1
+                i+=1
+                prev+=1
+            else:
+                if prev==0:
+                    lps[i]=0
+                    i+=1
+                else:
+                    prev=lps[prev-1]
         i=0
-        match=True
+        j=0
         while i<len(haystack):
-            if haystack[i]==needle[0]:
-                for j in range(1, len(needle)):
-                    if i+j>=len(haystack):
-                        return -1
-                    if needle[j]!=haystack[i+j]:
-                        match=False
-                        break
-                if match:
-                    return i
-            match=True
-            i+=1
+            if haystack[i]==needle[j]:
+                i+=1
+                j+=1
+            else:
+                if j==0:
+                    i+=1
+                else:
+                    j=lps[j-1]
+            if j==len(needle):
+                return i-len(needle)
         return -1
         
