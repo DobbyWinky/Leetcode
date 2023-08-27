8269d52bb641afeb0160955d971243385e164da5
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2023-08-27 14:01:48 -0700

    Time: 115 ms (46.30%), Space: 28.9 MB (5.52%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def merge(self,l1, l2):
+    def merge(self, l1, l2):
         if l1==None and l2==None:
             return None
         if l1==None:
@@ -17,16 +17,18 @@ class Solution:
         else:
             l2.next=self.merge(l1, l2.next)
             return l2
+        
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if lists==None or len(lists)==0:
             return None
-        while (len(lists))>1:
-            mergedLists=[]
-            for i in range(0, len(lists), 2):
+        while len(lists)>1:
+            mergedList=[]
+            for i in range(0,len(lists), 2):
                 l1=lists[i]
                 l2=lists[i+1] if i+1<len(lists) else None
-                mergedLists.append(self.merge(l1, l2))
-            lists=mergedLists
+                mergedList.append(self.merge(l1, l2))
+            lists=mergedList
         return lists[0]
         
+            
         
