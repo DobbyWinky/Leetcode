5c966b1b1368b6a1040dcd1dc10b308c4b4aee92
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2023-08-27 18:33:24 -0700

    Time: 47 ms (52.27%), Space: 16.3 MB (82.96%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,12 +3,10 @@ class Solution:
         n=len(s)
         dp=[False]*(n+1)
         dp[n]=True
-        i=n-1
-        while i>=0:
+        for i in range(n-1, -1, -1):
             for word in wordDict:
-                if i+len(word)<=n and word==s[i:i+len(word)] and dp[i+len(word)]:
+                if i+len(word)<=n and dp[i+len(word)] and s[i:i+len(word)]==word:
                     dp[i]=True
-                    break
-            i-=1
         return dp[0]
-            
+                    
+        
