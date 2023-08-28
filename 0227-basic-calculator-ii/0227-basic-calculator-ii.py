6bf8f5a5904bc616c264b5253e85eb95dcdc3b7c
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2023-08-27 22:36:41 -0500

    Time: 172 ms (37.35%), Space: 18.5 MB (97.83%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,33 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        curr=res=prev=0
-        curr_sign='+'
         i=0
-        n=len(s)
-        while i<n:
+        curr=0
+        curr_sign='+'
+        ans=0
+        while i<len(s):
             if s[i].isdigit():
-                while i<n and s[i].isdigit():
+                while i<len(s) and s[i].isdigit():
                     curr=curr*10+int(s[i])
                     i+=1
                 i-=1
                 if curr_sign=='+':
-                    res+=curr
+                    ans+=curr
                     prev=curr
                 elif curr_sign=='-':
-                    res-=curr
+                    ans-=curr
                     prev=-curr
                 elif curr_sign=='*':
-                    res-=prev
-                    res+=prev*curr
+                    ans-=prev
+                    ans+=prev*curr
                     prev=prev*curr
-                elif curr_sign=='/':
-                    res-=prev
-                    res+=int(prev/curr)
+                    
+                else:
+                    ans-=prev
+                    ans+=int(prev/curr)
                     prev=int(prev/curr)
                 curr=0
             elif s[i]!=' ':
                 curr_sign=s[i]
-                
             i+=1
-        return res
+        return ans
+                    
+            
         
