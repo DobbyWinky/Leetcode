b07415763282f4a5cba515adf34baa5cc62c4171
Madhangi Rathinasabapathi <34759319+DobbyWinky@users.noreply.github.com>
2024-05-03 11:42:35 -0500

    Time: 1126 ms (27.73%), Space: 21.4 MB (9.97%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
         ans=set()
-        ansList=[]
-        for i in range(n):
-            hash=set()
-            for j in range(i+1, n):
-                if -(nums[i]+nums[j]) in hash:
-                    ans.add(tuple(sorted((nums[i], nums[j], -(nums[i]+nums[j])))))
-                else:
-                    hash.add(nums[j])
-        for k in ans:
-            ansList.append(k)
-        return ansList
-            
+        dups=set()
+        hash=collections.defaultdict(int)
         
+        n=len(nums)
+        for i in range(n):
+            if nums[i] not in dups:
+                dups.add(nums[i])
+                for j in range(i+1, n):
+                    if -(nums[i]+nums[j]) in hash and hash[-(nums[i]+nums[j])]!=i and hash[-(nums[i]+nums[j])]!=j:
+                        ans.add(tuple(sorted((nums[i], nums[j], -(nums[i]+nums[j])))))
+                    hash[nums[j]]=j
+        return ans
+                
